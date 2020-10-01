1e2024cf9d78f10313d75aebea887c38e8e67b0d
carl-33 <koepkecj@gmail.com>
2020-10-01 00:08:27 -0500

    added name and id to textarea html elements

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// add/change class of ".hour" to ".past" ".present" or ".future" depending on the current time
+// add/change class of ".hour" to ".past" ".present" or ".future" depending on the current time  DONE
 
 
 // add to local storage when save button is clicked
 // pull from local storage when page is reloaded
 
-// display the day at "#current-day"
+// display the day at "#current-day" DONE
 
 
 // declare variables
+
+$(document).ready(function() {
+
+
 var saveBtn = $(".saveBtn")
 var currentDay = $("#currentDay")
 var nineAM = $('#9AM')
@@ -19,13 +23,53 @@ var twoPM = $("#2PM")
 var threePM = $("#3PM")
 var fourPM = $("#4PM")
 var fivePM = $("#5PM")
+var timeBlock = $(".time-block")
+var save9 = $("#save-button-9AM")
+var save10 = $("#save-button-10AM")
+var save11 = $("#save-button-11AM")
+var save12 = $("#save-button-12PM")
+var save1 = $("#save-button-1PM")
+var save2 = $("#save-button-2PM")
+var save3 = $("#save-button-3PM")
+var save4 = $("#save-button-4PM")
+var save5 = $("#save-button-5PM")
+var textArea9 = $("#text-area-9AM")
+var textArea10 = $("#text-area-10AM")
+var textArea11 = $("#text-area-11AM")
+var textArea12 = $("#text-area-12PM")
+var textArea1 = $("#text-area-1PM")
+var textArea2 = $("#text-area-2PM")
+var textArea3 = $("#text-area-3PM")
+var textArea4 = $("#text-area-4PM")
+var textArea5 = $("#text-area-5PM")
 
+var textAreaArray = [textArea9, textArea10, textArea11, textArea12, textArea1, textArea2, textArea3, textArea4, textArea5]
+var saveBtnArray = [save9, save10, save11, save12, save1, save2, save3, save4, save5]
 var plannerRow = $(".planner-row")
 
 // var hour = 18
 var hour = moment().format('H');
 console.log("the hour is " + hour)
 currentDay.append(moment().format('MMMM Do YYYY'));
+
+
+save9.on("click",function(event) {
+    event.preventDefault();
+
+    var schedule = textArea9.text;
+    localStorage.setItem("schedule", schedule);
+    console.log(schedule)
+
+    renderPlanner();
+
+});
+
+function renderPlanner () {
+    var schedule = localStorage.getItem("Schedule" )
+
+    timeBlock.textContent = schedule
+};
+
 
 if (hour < 9 ){
     plannerRow.removeClass("past")
@@ -161,3 +205,7 @@ if (hour > 17) {
     plannerRow.removeClass("future")
     plannerRow.addClass("past")
 }
+
+
+
+});
